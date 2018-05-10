cace17f3d95845451c1d99564546c7a6d15b4a81
shimba <tshimba@jp.ibm.com>
2018-05-10 14:29:05 +0900

    first commit

---
+++ src/main/java/com/taikishimba/azure/PublicCloudEvaluationAzure/PublicCloudEvaluationAzureApplication.java
@@ -0,0 +1,12 @@
+package com.taikishimba.azure.PublicCloudEvaluationAzure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicCloudEvaluationAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicCloudEvaluationAzureApplication.class, args);
+	}
+}
